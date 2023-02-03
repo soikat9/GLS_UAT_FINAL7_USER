ccca271a9e8255ed71b214fe0499aed01de8adcf
inicoding <irfannurrahmat@outlook.com>
2023-02-03 11:22:11 +0700

    updt expense

--- sol_hr_expense/models/hr_expense.py
+++ sol_hr_expense/models/hr_expense.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class HrExpenseSheet(models.Model):
     _inherit = 'hr.expense.sheet'
@@ -6,3 +7,8 @@ class HrExpenseSheet(models.Model):
     # ref = fields.Char(string='Bill Reference', related='expense_line_ids.reference')
     # start_date = fields.Date(string='Plan Date')
     # end_date = fields.Date(string='End Date')
+    def _compute_attachment_number(self):
+        for sheet in self:
+            sheet.attachment_number = sum(sheet.expense_line_ids.mapped('attachment_number'))
+            if (len(sheet.attachment_number) == 0):
+                raise ValidationError("Attachment Must be Fill")
