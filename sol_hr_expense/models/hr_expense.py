978f981ef37809533d9e8f0c7db3d1aeb872a46c
inicoding <irfannurrahmat@outlook.com>
2023-02-03 11:25:52 +0700

    updt expense

--- sol_hr_expense/models/hr_expense.py
+++ sol_hr_expense/models/hr_expense.py
@@ -7,8 +7,8 @@ class HrExpenseSheet(models.Model):
     # ref = fields.Char(string='Bill Reference', related='expense_line_ids.reference')
     # start_date = fields.Date(string='Plan Date')
     # end_date = fields.Date(string='End Date')
-    def _compute_attachment_number(self):
-        for sheet in self:
-            sheet.attachment_number = sum(sheet.expense_line_ids.mapped('attachment_number'))
-            if sheet.attachment_number == 0:
-                raise ValidationError("Attachment Must be Fill")
+    # def _compute_attachment_number(self):
+    #     for sheet in self:
+    #         sheet.attachment_number = sum(sheet.expense_line_ids.mapped('attachment_number'))
+    #         if sheet.attachment_number == 0:
+    #             raise ValidationError("Attachment Must be Fill")
