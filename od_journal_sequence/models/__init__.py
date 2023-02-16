46a6811752bfba2f088859da6d21ea1303baeb11
hamsky13 <ilham.ius13@gmail.com>
2023-02-16 21:40:34 +0700

    tes

--- od_journal_sequence/models/__init__.py
+++ od_journal_sequence/models/__init__.py
@@ -2,5 +2,5 @@
 
 from . import account_journal
 from . import account_move
-# from . import account_bank_statement
+from . import account_bank_statement
 
--- od_journal_sequence/models/account_bank_statement.py
+++ od_journal_sequence/models/account_bank_statement.py
@@ -2,31 +2,12 @@ from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 class AccountBankStatement(models.Model):
-  _inherit = 'account.bank.statement'
+    _inherit = 'account.bank.statement'
 
-  name = fields.Char(compute="_compute_name_by_sequence",
-                     string='Reference',
-                     states={'open': [('readonly', False)]},
-                     copy=False,
-                     readonly=True)
+    name = fields.Char(string='Reference',
+                       states={'open': [('readonly', False)]},
+                       copy=False,
+                       readonly=True,
+                       related='move_id.name',
+                       store=True)
 
-  @api.depends("state", "journal_id", "date")
-  def _compute_name_by_sequence(self):
-      for line in self:
-          name = line.name or "/"
-          if (
-                    line.state == "posted"
-                    and (not line.name or line.name == "/")
-                    and line.journal_id
-                    and line.journal_id.sequence_id
-                    and line.journal_id.out_sequence_id
-            ):
-                if line.cash_type == "receipt":
-                    seq = line.journal_id.sequence_id
-                elif line.cash_type == "disbursment":
-                    seq = line.journal_id.out_sequence_id
-                name = seq.next_by_id(sequence_date=line.date)
-          line.name = name
-
-  def _constrains_date_sequence(self):
-     return True
